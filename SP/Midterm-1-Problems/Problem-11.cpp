bfa4747c4814e84d926f91421de831e3e4f5d81c
Marko Ivanovski <131307678+Marko-Ivanovski@users.noreply.github.com>
2024-01-16 02:53:46 +0100

    Zig-Zag 2
    A zigzag is a number for which it is true that for every pair of adjacent digits in the number, one is less than 5 and the other is greater than or equal to 5. For example, if the number x is composed of the digits a, b, c and d , then the following holds for them:
    
    a<5, b>=5, c<5, d>=5 or a>=5, b<5, c>=5, d<5

---
+++ SP/Midterm-1-Problems/Problem-11.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+    int x, br = 0, tmp, cifra1, cifra2, p, f;
+
+    while(scanf("%d", &x))
+    {
+        f=1;
+        if(x>=10){
+            tmp = x;
+            cifra1 = tmp%10;
+            tmp/=10;
+            cifra2 = tmp%10;
+            tmp/=10;
+            if(cifra1 > cifra2)
+            {
+                p=1;
+            }
+            else if(cifra1 < cifra2)
+            {
+                p=0;
+            }
+            else
+                f = 0;
+            cifra1 = cifra2;
+            if(f)
+            {
+                while(tmp) {
+                    cifra2 = tmp%10;
+                    if(p){
+                        if(cifra1>=cifra2){
+                            f=0;
+                            break;
+                        }
+                        else
+                            p = 0;
+                    }
+                    else{
+                    	if(cifra1<=cifra2){
+                            f=0;
+                            break;
+                        }
+                        else
+                            p = 1;
+                    }
+                    tmp /= 10;
+                    cifra1 = cifra2;
+                }
+            }
+            if(f)
+                printf("%d\n", x);
+        }
+
+    }
+
+    return 0;
+}
