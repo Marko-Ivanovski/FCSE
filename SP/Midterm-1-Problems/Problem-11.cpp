ccc171391d778f33019ab6f0c56585d5a99ea919
Marko Ivanovski <131307678+Marko-Ivanovski@users.noreply.github.com>
2024-01-16 02:56:00 +0100

    Zig-Zag 2
    A zigzag is a number for which it is true that for every pair of adjacent digits in the number, one is less than 5 and the other is greater than or equal to 5. For example, if the number x is composed of the digits a, b, c and d , then the following holds for them:
    
    a<5, b>=5, c<5, d>=5 or a>=5, b<5, c>=5, d<5

--- SP/Midterm-1-Problems/Problem-11.cpp
+++ SP/Midterm-1-Problems/Problem-11.cpp
@@ -1,56 +1,46 @@
-#include <stdio.h>
+#include <iostream>
 
 int main() {
     int x, br = 0, tmp, cifra1, cifra2, p, f;
 
-    while(scanf("%d", &x))
-    {
-        f=1;
-        if(x>=10){
+    while (cin >> x) {
+        f = 1;
+        if (x >= 10) {
             tmp = x;
-            cifra1 = tmp%10;
-            tmp/=10;
-            cifra2 = tmp%10;
-            tmp/=10;
-            if(cifra1 > cifra2)
-            {
-                p=1;
-            }
-            else if(cifra1 < cifra2)
-            {
-                p=0;
-            }
-            else
+            cifra1 = tmp % 10;
+            tmp /= 10;
+            cifra2 = tmp % 10;
+            tmp /= 10;
+            if (cifra1 < 5 && cifra2 >= 5) {
+                p = 1;
+            } else if (cifra1 >= 5 && cifra2 < 5) {
+                p = 0;
+            } else
                 f = 0;
             cifra1 = cifra2;
-            if(f)
-            {
-                while(tmp) {
-                    cifra2 = tmp%10;
-                    if(p){
-                        if(cifra1>=cifra2){
-                            f=0;
+            if (f) {
+                while (tmp) {
+                    cifra2 = tmp % 10;
+                    if (p) {
+                        if (!(cifra1 >= 5 && cifra2 < 5)) {
+                            f = 0;
                             break;
-                        }
-                        else
+                        } else
                             p = 0;
-                    }
-                    else{
-                    	if(cifra1<=cifra2){
-                            f=0;
+                    } else {
+                        if (!(cifra1 < 5 && cifra2 >= 5)) {
+                            f = 0;
                             break;
-                        }
-                        else
+                        } else
                             p = 1;
                     }
                     tmp /= 10;
                     cifra1 = cifra2;
                 }
             }
-            if(f)
-                printf("%d\n", x);
+            if (f)
+                cout << x << endl;
         }
-
     }
 
     return 0;
