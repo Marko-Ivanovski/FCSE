47820e695a8667884a2e573c9ce03005b6d6a3ce
Marko Ivanovski <markoivanovski.mi@icloud.com>
2024-03-25 20:45:37 +0100

    If-Elif-Else conditionals

---
+++ OS/Auditory-Problems/Scripts/Problem-02.sh
@@ -0,0 +1,9 @@
+if [ "$1" = "1" ]; then
+    echo "Option 1 selected."
+elif [ "$1" = "2" ]; then
+    echo "Option 2 selected."
+elif [ "$1" = "3" ]; then
+    echo "Option 3 selected."
+else
+    echo "Invalid option."
+fi
